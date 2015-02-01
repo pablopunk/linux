5d982a934f967e9fcafbadfe4280d740795ec29e
Pablo Varela <pablovarela182@gmail.com>
2015-02-01 23:25:37 +0100

    Xmobar añadido

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,11 +1,19 @@
 import XMonad
- 
-main = xmonad defaultConfig
-        { borderWidth		= 2
-        , terminal		= "gnome-terminal"
-	, focusedBorderColor	= "#3388ee"
-	, normalBorderColor	= "#cccccc"
-	, focusFollowsMouse	= True
-        -- more changes
-        }
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
+
+main = do
+	xmproc <- spawnPipe "/usr/bin/xmobar /home/pol/.xmobarrc"
+	xmonad $ defaultConfig
+        	{ manageHook = manageDocks <+> manageHook defaultConfig
+        	, layoutHook = avoidStruts  $  layoutHook defaultConfig
+		, borderWidth		= 2
+        	, terminal		= "gnome-terminal"
+		, focusedBorderColor	= "#3388ee"
+		, normalBorderColor	= "#cccccc"
+		, focusFollowsMouse	= True
+		}
 
